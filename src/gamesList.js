f8d378926536c95c35c0a9116b9236191e39f6e5
AdamSpannbauer <spannbaueradam@gmail.com>
2021-02-18 06:05:46 -0500

    add stackem to arcade

--- src/gamesList.js
+++ src/gamesList.js
@@ -1,9 +1,10 @@
-import { FindTheDude } from "./games/findTheDude/miniGameClass.js";
-import { MemoryGame } from "./games/memoryGame/miniGameClass.js";
-import { PopupCloser } from "./games/popupCloser/miniGameClass.js";
-import { ProtectTheCenter } from "./games/protectTheCenter/miniGameClass.js";
-import { SpotTheDifference } from "./games/spotTheDifference/miniGameClass.js";
-import { WaterGunRace } from "./games/waterGunRace/waterGunRace.js";
+import { FindTheDude } from './games/findTheDude/miniGameClass.js';
+import { MemoryGame } from './games/memoryGame/miniGameClass.js';
+import { PopupCloser } from './games/popupCloser/miniGameClass.js';
+import { ProtectTheCenter } from './games/protectTheCenter/miniGameClass.js';
+import { SpotTheDifference } from './games/spotTheDifference/miniGameClass.js';
+import { StackEm } from './games/stackEm/miniGameClass.js';
+import { WaterGunRace } from './games/waterGunRace/waterGunRace.js';
 
 export const GAMES_LIST = [
   WaterGunRace,
@@ -12,4 +13,5 @@ export const GAMES_LIST = [
   ProtectTheCenter,
   MemoryGame,
   PopupCloser,
+  StackEm,
 ];
