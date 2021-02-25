da8fea0ddfda97243c6e943972126a5d8c8c2288
Adam Spannbauer <spannbaueradam@gmail.com>
2021-02-25 06:19:08 -0500

    reskin dude to be 1 of 10 random icons

--- src/games/findTheDude/miniGameClass.js
+++ src/games/findTheDude/miniGameClass.js
@@ -16,11 +16,26 @@ export class FindTheDude extends MiniGame {
     this.dudeY = random(height);
     this.dudeR = 15;
     this.spotlightR = width * 0.6;
+
+    this.dudeIcons = [
+      '\uf535', // kiwi bird
+      '\uf6ed', // hippo
+      '\uf182', // woman
+      '\uf183', // man
+      '\uf6d5', // dragon
+      '\uf6e2', // ghost
+      '\uf29d', // blind man
+      '\uf77c', // baby
+      '\uf6e2', // ghost
+      '\uf6be', // cat
+    ];
+    this.dudeIcon = random(this.dudeIcons);
   }
 
   resetGame() {
     this.dudeX = random(width);
     this.dudeY = random(height);
+    this.dudeIcon = random(this.dudeIcons);
   }
 
   checkFound() {
@@ -34,6 +49,19 @@ export class FindTheDude extends MiniGame {
     this.checkFound();
   }
 
+  drawDude() {
+    push();
+    fill(100, 255, 100);
+    strokeWeight(3);
+
+    textFont(this.fontAwesome);
+    textSize(this.dudeR * 2);
+    textAlign(CENTER, CENTER);
+
+    text(this.dudeIcon, this.dudeX, this.dudeY);
+    pop();
+  }
+
   draw() {
     push();
     background(0);
@@ -45,8 +73,7 @@ export class FindTheDude extends MiniGame {
     // Draw dude if close-ish to avoid
     // accidentally showing dude outside of spotligh
     if (dist(mouseX, mouseY, this.dudeX, this.dudeY) < 300) {
-      fill(0, 255, 0);
-      ellipse(this.dudeX, this.dudeY, this.dudeR * 2);
+      this.drawDude();
     }
 
     // Mask image besides spotlight area
